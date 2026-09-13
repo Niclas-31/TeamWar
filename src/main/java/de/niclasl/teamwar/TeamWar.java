271c6ce8d3ad86eb9e6282edd20aca1b47f784fa
Niclas-31 <niclasloeffler3105@gmail.com>
2026-09-13 09:55:54 +0200

    Initial commit

--- src/main/java/de/niclasl/teamwar/TeamWar.java
+++ src/main/java/de/niclasl/teamwar/TeamWar.java
@@ -1,9 +1,20 @@
 package de.niclasl.teamwar;
 
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import org.slf4j.Logger;
 
 @Mod(TeamWar.MOD_ID)
 public class TeamWar {
-
     public static final String MOD_ID = "teamwar";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public TeamWar(IEventBus modEventBus, ModContainer modContainer) {
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
 }
